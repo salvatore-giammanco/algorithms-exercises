b53913edfe824d88270708116559ea46a8dd61ca
Salvatore Giammanco <salvatore@giammanco.me>
2022-06-22 08:53:23 +0200

    [add] bubble sort implementation in Rust

---
+++ exercises/bubble-sort/bubble-sort.rs
@@ -0,0 +1,18 @@
+
+fn bubble_sort(to_sort: &mut [u32]) {
+    // In place bubble sort algorithm
+    let len: usize = to_sort.len();
+    for i in (1..len).rev() {
+        for j in (len - i..len).rev() {
+            if to_sort[j - 1] > to_sort[j] {
+                (to_sort[j - 1], to_sort[j]) = (to_sort[j], to_sort[j - 1])
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut input = vec![92, 3, 199, 9, 10, 23, 42, 0, 71, 1];
+    bubble_sort(&mut input);
+    println!("{:?}", input);
+}
