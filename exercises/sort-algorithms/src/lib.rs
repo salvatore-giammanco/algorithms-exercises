a01281460a8fe640a1c67dcd13acb2ba05dae879
Salvatore Giammanco <salvatore@giammanco.me>
2022-06-30 20:56:34 +0200

    [move] bubble sort and selection sort algorithms into proper crate

--- exercises/sort-algorithms/src/lib.rs
+++ exercises/sort-algorithms/src/lib.rs
@@ -1,10 +1,34 @@
-pub fn insertion_sort(to_sort: &mut Vec<u32>){
+pub fn insertion_sort(to_sort: &mut Vec<u32>) {
     for i in 0..to_sort.len() {
         let mut j: usize = i;
         while j > 0 && to_sort[j] < to_sort[j - 1] {
             to_sort.swap(j, j - 1);
             j -= 1;
         }
+    }
+}
+
+pub fn bubble_sort(to_sort: &mut Vec<u32>) {
+    let len = to_sort.len();
+    for i in (1..len).rev() {
+        for j in (len - i..len).rev() {
+            if to_sort[j - 1] > to_sort[j] {
+                to_sort.swap(j, j - 1);
+            }
+        }
+    }
+}
+
+pub fn selection_sort(to_sort: &mut Vec<u32>) {
+    let len = to_sort.len();
+    for i in 0..len {
+        let mut min_index = i;
+        for j in i..len {
+            if to_sort[j] < to_sort[min_index] {
+                min_index = j;
+            }
+        }
+        to_sort.swap(i, min_index);
     }
 }
 
@@ -28,6 +52,22 @@ mod tests {
     fn test_insertion_sort() {
         let mut test_data = test_setup();
         insertion_sort(&mut test_data.to_sort);
+        
+        assert_eq!(test_data.to_sort, test_data.sorted);
+    }
+
+    #[test]
+    fn test_bubble_sort() {
+        let mut test_data = test_setup();
+        bubble_sort(&mut test_data.to_sort);
+        
+        assert_eq!(test_data.to_sort, test_data.sorted);
+    }
+
+    #[test]
+    fn test_selection_sort() {
+        let mut test_data = test_setup();
+        selection_sort(&mut test_data.to_sort);
         
         assert_eq!(test_data.to_sort, test_data.sorted);
     }
